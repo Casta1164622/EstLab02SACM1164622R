308fd9a6c30ac88364bf47121de31a98645d4114
Casta1164622 <sacastanedam@correo.url.edu.gt>
2023-04-24 19:01:44 -0600

    Obtencion del cliente ganador de la auction completado

--- EstLab02SACM1164622/customerPriority.py
+++ EstLab02SACM1164622/customerPriority.py
@@ -1,13 +1,10 @@
 import heapSort as heap
 def orderCustomerPriority(customerList):
-
-    BudgetList = []
-    for i in range(len(customerList)):
-        BudgetList.append(customerList[i]['budget'])
-
     heap.heapSort(customerList)
     return customerList
 
+def getCustomer(priorityList,rejections):
+    return priorityList[rejections]
 
 
 
--- EstLab02SACM1164622/main.py
+++ EstLab02SACM1164622/main.py
@@ -1,11 +1,11 @@
 import jsonReader as jreader
-import heapSort as heap
 import customerPriority as customerP
 
 Customers = jreader.readJsonFile('inputs/input_customer_example_lab_3.jsonl')
 Auctions = jreader.readJsonFile('inputs/input_auctions_example_lab_3.jsonl')
 
 for i in range(len(Auctions)):
-    data1 =customerP.orderCustomerPriority(Auctions[i]['customers'])
+    OrderedCustomerList = customerP.orderCustomerPriority(Auctions[i]['customers'])
+    AuctionWinner = customerP.getCustomer(OrderedCustomerList,Auctions[i]['rejection'])
     print(i)
 
