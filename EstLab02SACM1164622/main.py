fa89e272e2b3b31e9c68acf1ffc40f3ca739bb5c
Casta1164622 <sacastanedam@correo.url.edu.gt>
2023-04-24 18:18:37 -0600

    Agregado funcion HeapSort con MinHeap para ordenar de mayor a menor

--- EstLab02SACM1164622/main.py
+++ EstLab02SACM1164622/main.py
@@ -1,7 +1,13 @@
 import jsonReader as jreader
+import heapSort as heap
 
 Customers = jreader.readJsonFile('inputs/input_customer_example_lab_3.jsonl')
 Auctions = jreader.readJsonFile('inputs/input_auctions_example_lab_3.jsonl')
-for i in range(len(Customers)):
+
+arr = [1,42,42,542,5,53,52,245,245,56,631]
+heap.heapSort(arr)
+
+
+for i in range(len(Auctions)):
    print(i)
 
